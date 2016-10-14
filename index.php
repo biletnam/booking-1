b9a95a92fac88f79f076c3f0ad3ffae800d800a7
Alexis Nootens <me@axn.io>
2016-10-14 11:49:22 +0200

    Structurations et vérifications

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
-/* Template du corps de la page d'accueil. */
-$body_tpl = <<<EOT
+// Initialisation d'une session.
+session_start();
+
+// Déclaration des templates
+GLOBAL $_TPL_HOME, $_TPL_DETAILS;
+
+// Assignation des tamples
+$TPL_HOME = <<<EOT
 	<h2>Reservation</h2>
 	<p>Le prix de la place est de 10€ jusqu'à 12 ans et ensuite de 15€.</p>
 	<p>Le prix de l'assurance annulation est de 20€ quel ue soit le nombre de voyageurs.</p>
@@ -19,47 +25,81 @@ $body_tpl = <<<EOT
 			</tr>
 			<tr>
 				<th>Assurance annulation</th>
-				<th>**</th>
+				<th><input type="checkbox" name="insurance" value="checked" {$_GET['insurance']}></th>
 			</tr>
 			<tr>
 				<th><button type="submit">Étape suivante</button></th>
-				<th><button type="submit" formaction="cancel.php">Annuler la réservation</button></th>
+				<th><button type="submit" name="cancelled">Annuler la réservation</button></th>
 			</tr>
 		</table>
 		</fieldset>
 	</form>
 EOT;
 
-$details_tpl = <<<EOT
-<h2>Détail des réservations</h2>
+$TPL_DETAILS = <<<EOT
+	<h2>Détail des réservations</h2>
 
-<table>
-	<tr>
-		<th>Nom</th>
-		<th>><input type="text" name="name"></th>
-	</tr>
-	<tr>
-		<th>Age</th>
-		<th>><input type="text" name="age"></th>
-	</tr>
-</table>
+	<table>
+		<tr>
+			<th>Nom</th>
+			<th>><input type="text" name="name"></th>
+		</tr>
+		<tr>
+			<th>Age</th>
+			<th>><input type="text" name="age"></th>
+		</tr>
+	</table>
 EOT;
 
-if (isset($_GET['destination']) AND
-	isset($_GET['persons_counter']))
+function checkForInput()
 {
+	/* Vérification sur les paramètres ont été passés
+	 * dans l'url.
+	 */
+	if (isset($_GET['destination']) AND	isset($_GET['persons_counter']) AND
+		isset($_GET['insurance']))
+	{
+		// S'ils ne sont pas VIDES !
+		if ($_GET['destination'] AND $_GET['persons_counter'] AND $_GET['insurance'])
+		{
+			$_SESSION['destination'] = $_GET['destination'];
+			$_SESSION['persons_counter'] = $_GET['persons_counter'];
+			$_SESSION['insurance'] = $_GET['insurance'];
 
-	if ($_GET['destination'] AND
-		$_GET['persons_counter'])
-	{
-		echo "Hello";
+			return true;
+		}
+		else
+		{
+			echo "Veuillez remplir tout les champs";
+		}
 	}
-	else
-	{
-		echo "Veuillez remplir tout les champs";
-	}
+
+	return false;
 }
 
-print ($body_tpl);
+function main()
+{
+	GLOBAL $TPL_HOME;
+	/* Si l'utilisateur annule sa réservation, la session est supprimée
+	 * et l'utilisateur est renvoyé vers l'acceuil sans paramètres passés.
+	 */
+	if (isset($_GET['cancelled']))
+	{
+		unset($_SESSION['destination']);
+		unset($_SESSION['persons_counter']);
+		unset($_SESSION['insurance']);
+		header('Location: index.php');
+	}
+
+	if (checkForInput())
+		goDetails();
+
+	// Affichage par défaut
+	print($TPL_HOME);
+
+	return 0;
+}
+
+main();
 
 ?>
