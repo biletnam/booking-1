3449ddce34015658c51a381f96cb2af9196dc0b3
Alexis Nootens <me@axn.io>
2016-10-15 16:04:15 +0200

    adding code structure

--- index.php
+++ index.php
@@ -9,29 +9,28 @@ GLOBAL $TPL_HOME, $TPL_DETAILS, $TPL_VALIDATION, $TPL_CONFIRMATION;
 /* Les templates sont stockés dans des variables par la technique des `heredoc`
  * car cette technique protège de la corruption du texte tout en autorisant les
  * caractères d'échappement.
- * Deux formes de marqueur sont présent : %variable% et {$_POST['xxx']}
- * %variable% sert de marqueur à str_replace() et {$_POST['xxx']} est automatiquement
+ * Deux formes de marqueur sont présent : %variable% et {$_SESSION['xxx']}
+ * %variable% sert de marqueur à str_replace() et {$_SESSION['xxx']} est automatiquement
  * inséré par php si la variable existe.
  */
 $TPL_HOME = <<<EOT
-    <h2>Reservation</h2>
+    <h2>Réservation</h2>
     <p>Le prix de la place est de 10€ jusqu'à 12 ans et ensuite de 15€.</p>
     <p>Le prix de l'assurance annulation est de 20€ quel que soit le nombre de voyageurs.</p>
-
     <form method="post">
     <fieldset>
         <table>
             <tr>
                 <th>Destination</th>
-                <th><input type="text" name="destination" value="{$_POST['destination']}"></th>
+                <th><input type="text" name="destination" value="{$_SESSION['destination']}"></th>
             </tr>
             <tr>
                 <th>Nombre de places</th>
-                <th><input type="text" name="persons_counter" value="{$_POST['persons_counter']}"></th>
+                <th><input type="text" name="persons_counter" value="{$_SESSION['persons_counter']}"></th>
             </tr>
             <tr>
                 <th>Assurance annulation</th>
-                <th><input type="checkbox" name="insurance" value="checked" {$_POST['insurance']}></th>
+                <th><input type="checkbox" name="insurance" value="checked" {$_SESSION['insurance']}></th>
             </tr>
         </table>
     </fieldset>
@@ -42,14 +41,13 @@ EOT;
 
 $TPL_DETAILS = <<<EOT
     <h2>Détail des réservations</h2>
-
     <form method="post">
     <fieldset>
         <table>
             %table%
         </table>
     </fieldset>
-    <button type="submit">Étape suivante</button>
+    <button type="submit" name="page" value="3">Étape suivante</button>
     <button type="submit" name="page" value="1">Retour à la page précédente</button>
     <button type="submit" name="reset">Annuler la réservation</button>
     </form>
@@ -57,14 +55,13 @@ EOT;
 
 $TPL_VALIDATION = <<<EOT
     <h2>Validation des réservations</h2>
-
     <form method="post">
     <fieldset>
         <table>
             %table%
         </table>
     </fieldset>
-    <button type="submit">Étape suivante</button>
+    <button type="submit" name="page" value="4">Étape suivante</button>
     <button type="submit" name="page" value="2">Retour à la page précédente</button>
     <button type="submit" name="reset">Annuler la réservation</button>
     </form>
@@ -72,7 +69,6 @@ EOT;
 
 $TPL_CONFIRMATION = <<<EOT
     <h2>Confirmation des réservations</h2>
-
     <form method="post">
     <p>Votre demande a bien été enregistrée.</p>
     <p>Merci de bien vouloir verser la somme de x€ sur le compte 000-000000-00</p>
@@ -81,11 +77,11 @@ $TPL_CONFIRMATION = <<<EOT
 EOT;
 
 /**
- * Vérifie la validité des données transmises.
+ * Vérifie la validité des données transmises à la page d'accueil.
  * @param none
  * @return true si les données sont valides, false autrement.
  */
-function checkForInput()
+function checkForm1()
 {
     // les variables existent *ET* ne sont pas vides ?
     if (!empty($_POST['destination']) AND !empty($_POST['persons_counter']))
@@ -107,12 +103,40 @@ function checkForInput()
 }
 
 /**
+ * Vérifie la validité des données transmises à la page de détails.
+ * @param none
+ * @return true si les données sont valides, false autrement.
+ */
+function checkForm2()
+{
+    /*
+    // les variables existent *ET* ne sont pas vides ?
+    if (!empty($_POST['destination']) AND !empty($_POST['persons_counter']))
+    {
+        $_SESSION['destination'] = htmlspecialchars($_POST['destination']);
+        $_SESSION['persons_counter'] = intval($_POST['persons_counter']);
+        $_SESSION['insurance'] = isset($_POST['insurance']);
+
+        return true;
+    }
+
+    // ou les variables existent simplement ?
+    elseif (isset($_POST['destination']) AND isset($_POST['persons_counter']))
+    {
+        print("Veuillez remplir tout les champs correctement.");
+    }
+    */
+
+    return false;
+}
+
+/**
  * Complète le nombre de case nécessaires au tableau des détails
  * et affiche la page générée.
  * @param none
  * @return none
  */
-function goDetails()
+function generateDetails()
 {
     GLOBAL $TPL_DETAILS;
 
@@ -120,19 +144,41 @@ function goDetails()
 
     for ($i = 0; $i < intval($_SESSION['persons_counter']); $i++)
     {
-        $table .= "<tr>" .
-                  "  <th>Nom</th>" .
-                  "  <th><input type=\"text\" name=\"fullname\"></th>" .
-                  "</tr>" .
-                  "<tr>" .
-                  "  <th>Age</th>" .
-                  "  <th><input type=\"text\" name=\"age\"></th>" .
-                  "</tr>";
+        $table .=<<<EOD
+        <tr>
+            <th>Nom</th>
+            <th><input type="text" name="fullname[]"></th>
+        </tr>
+        <tr>
+            <th>Age</th>
+            <th><input type="text" name="age[]"></th>
+        </tr>;
+EOD;
     }
 
     $TPL_DETAILS = str_replace("%table%", $table, $TPL_DETAILS);
 
     print($TPL_DETAILS);
+}
+
+/**
+ * Génère la page récapitulative de la réservation et l'affiche.
+ * @param none
+ * @return none
+ */
+function generateValidation()
+{
+    //TODO
+}
+
+/**
+ * Génère la page de confirmation en indiquant le prix à payer et l'affiche.
+ * @param none
+ * @return none
+ */
+function generateConfirmation()
+{
+    //TODO
 }
 
 
@@ -146,18 +192,28 @@ function main()
      */
     if (isset($_POST['reset']))
     {
+        session_unset();
+        session_destroy();
+        session_regenerate_id(true);
         header('Location:#');
-        die();
     }
 
     switch ($_POST['page']) {
-        case '3':
+        case '4':
+            generateConfirmation();
             break;
 
-        case '2':
-            if (checkForInput())
+        case '3':
+            if (checkForm2())
             {
-                goDetails();
+                generateValidation();
+                break;
+            }
+
+        case '2':
+            if (checkForm1())
+            {
+                generateDetails();
                 break;
             }
 
