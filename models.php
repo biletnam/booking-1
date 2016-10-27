biletnam/booking-1

commit be57521d098f7500bae8e9e207cde5f012eb08b7

models: create save function

models.php
@@ -57,6 +57,16 @@ class Reservation
         $this->persons = array();
     }
 
+    /**
+     * Save the current instance of Reservation to the session.
+     * @param none
+     * @return none
+     */
+    public function save()
+    {
+        $_SESSION['reservation'] = serialize($reservation);
+    }
+
 }
 
 ?>

controllers.php
@@ -62,7 +62,7 @@ function check_form_home($reservation)
         $reservation->insurance = isset($_POST['insurance']);
 
         // don't forget to save!! (-_-;)
-        $_SESSION['reservation'] = serialize($reservation);
+        $reservation->save();
 
         return true;
     }

commit c84f487b99d13bffc08e355d8d67535d9f38041e

models: fix bug introduced in be57521

models.php
@@ -64,7 +64,7 @@ class Reservation
      */
     public function save()
     {
-        $_SESSION['reservation'] = serialize($reservation);
+        $_SESSION['reservation'] = serialize($this);
     }
 
 }