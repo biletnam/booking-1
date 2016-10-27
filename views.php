biletnam/booking-1

commit 519eb49a1a7224c68f14c7f365787d083fa8a9c0

views: simplify organisation

views.php
@@ -8,66 +8,39 @@ use Models\Reservation as Reservation;
  * @param
  * @return
  */
-function vw_home($reservation)
+function vw_display($reservation, $page)
 {
-    print_header();
-    $tpl_home = file_get_contents('./templates/home.html');
-    generate_home($reservation, $tpl_home);
-    print_footer();
+    pr_chunk('header');
+    $template = file_get_contents('./templates/'.$page);
+
+    switch ($page) {
+        case 'home':
+            generate_home($reservation, $template);
+            break;
+
+        case 'details':
+            generate_details($reservation, $template);
+            break;
+
+        case 'validation':
+            generate_validation($reservation, $template);
+            break;
+
+        case 'confirmation':
+            generate_confirmation($reservation, $template);
+            break;
+    }
+
+    pr_chunk('footer');
 }
 
 /**
  * @param
  * @return
  */
-function vw_details($reservation)
+function pr_chunk($chunk)
 {
-    print_header();
-    $tpl_details = file_get_contents('./templates/details.html');
-    generate_details($reservation, $tpl_details);
-    print_footer();
-}
-
-/**
- * @param
- * @return
- */
-function vw_validation($reservation)
-{
-    print_header();
-    $tpl_validation = file_get_contents('./templates/validation.html');
-    generate_validation($reservation, $tpl_validation);
-    print_footer();
-}
-
-/**
- * @param
- * @return
- */
-function vw_confirmation($reservation)
-{
-    print_header();
-    $tpl_confirmation = file_get_contents('./templates/confirmation.html');
-    generate_confirmation($reservation, $tpl_confirmation);
-    print_footer();
-}
-
-/**
- * @param
- * @return
- */
-function print_header()
-{
-    print(file_get_contents('./templates/header.html'));
-}
-
-/**
- * @param
- * @return
- */
-function print_footer()
-{
-    print(file_get_contents('./templates/footer.html'));
+    print(file_get_contents('./templates/'.$chunk.'.html'));
 }
 
 /**

controllers.php
@@ -12,7 +12,7 @@ use Models\Reservation as Reservation;
 function ctr_home($reservation)
 {
     // not much to do here
-    vw_home($reservation);
+    vw_display($reservation, 'home');
 }
 
 /**
@@ -22,7 +22,7 @@ function ctr_home($reservation)
 function ctr_details($reservation)
 {
     if (check_form_home($reservation))
-        vw_details($reservation);
+        vw_display($reservation, 'details');
     else
         ctr_home($reservation);
 }
@@ -34,7 +34,7 @@ function ctr_details($reservation)
 function ctr_validation($reservation)
 {
     if (check_form_details($reservation))
-        vw_validation($reservation);
+        vw_display($reservation, 'validation');
     else
         ctr_details($reservation);
 }
@@ -45,7 +45,7 @@ function ctr_validation($reservation)
  */
 function ctr_confirmation($reservation)
 {
-    vw_confirmation($reservation);
+    vw_display($reservation, 'confirmation');
 }
 
 /**

commit f3d25217f4d904af307210275e5ae4ad4c5ae973

views: fix bug introduced in 519eb49

views.php
@@ -11,7 +11,7 @@ use Models\Reservation as Reservation;
 function vw_display($reservation, $page)
 {
     pr_chunk('header');
-    $template = file_get_contents('./templates/'.$page);
+    $template = file_get_contents('./templates/'.$page.'.html');
 
     switch ($page) {
         case 'home':