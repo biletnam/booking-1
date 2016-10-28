biletnam/booking-1

commit ec5ea30c36a420b865f73f2ab6eb018528a18027

controllers: refurb check_details

controllers.php
@@ -87,29 +87,25 @@ function check_form_details($reservation)
     // tables exist *AND* are not empty
     if (!empty($_POST['fullnames']) AND !empty($_POST['ages']))
     {
-        $fullnames = $_POST['fullnames'];
-        $ages = $_POST['ages'];
-        $validInput = true;
         $persons = array();
+        $ages = $_POST['ages'];
+        $count = count($fullnames);
+        $fullnames = $_POST['fullnames'];
 
-        for ($i = 0; $i < count($fullnames); $i++)
+        for ($i = 0; $i < $count; $i++)
         {
             // age is not 0 and fullname is set
             if ($ages[$i] AND $fullnames[$i])
-            {
                 array_push($persons, new Person($fullnames[$i], $ages[$i]));
-            }
             else
-            {
                 print("Veuillez remplir le champs ".$i." correctement.\n");
-                $invalidInput = false;
-            }
         }
 
         $reservation->persons = $persons;
         $reservation->save();
 
-        return $validInput;
+        // check if every fullname+age has an object in $persons
+        return $count == count($persons);
     }
 
     // or the variables just exist ?

commit ee5c48e6e81b9568cb29a744eb021988beec0305

controllers: fix bug introduced in ec5ea30

controllers.php
@@ -87,10 +87,10 @@ function check_form_details($reservation)
     // tables exist *AND* are not empty
     if (!empty($_POST['fullnames']) AND !empty($_POST['ages']))
     {
-        $persons = array();
         $ages = $_POST['ages'];
-        $count = count($fullnames);
         $fullnames = $_POST['fullnames'];
+        $persons = array();
+        $count = count($fullnames);
 
         for ($i = 0; $i < $count; $i++)
         {