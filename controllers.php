biletnam/booking-1

commit af69e9eb25c2f594f7a18361034d1bbc354bd94d

controllers: really fix the going back issue from 42d405a
Also remove the useless variable `$count`.

controllers.php
@@ -20,7 +20,7 @@ function redirect_control($reservation, $redirection)
         },
 
         'details' => function($reservation, $redirection) {
-            if (!check_form_home($reservation) AND empty($reservation->persons))
+            if (!check_form_home($reservation))    // if the informations are incorrect,
                 $redirection = 'home';             // return to the previous page.
             vw_display($reservation, $redirection);
         },
@@ -68,6 +68,9 @@ function check_form_home($reservation)
         }
     }
 
+    if (empty($reservation->persons))
+        return true; // we're coming from the next page and the datas are corrects
+
     $reservation->append_warning("Veuillez remplir tous les champs correctement.\n");
 
     return false;
@@ -85,10 +88,9 @@ function check_form_details($reservation)
     {
         $ages      = $_POST['ages'];
         $fullnames = $_POST['fullnames'];
-        $count     = count($fullnames);
         $persons   = array();
 
-        for ($i = 0; $i < $count; $i++)
+        for ($i = 0; $i < count($fullnames); $i++)
         {
             // age in [1;120] and fullname is set
             if (1 <= $ages[$i] AND $ages[$i] <= 120 AND $fullnames[$i])

commit bc2f0b69b3c5207313add7e6f6df0ee718b814fb

controllers models: fix bug introduced in af69e9e and userproofing
Replace empty() with count() for asserting that the persons table
is empty in check_form_home.
Save the age as integer instead of string in Persons.

controllers.php
@@ -68,7 +68,7 @@ function check_form_home($reservation)
         }
     }
 
-    if (empty($reservation->persons))
+    if (count($reservation->persons) != 0)
         return true; // we're coming from the next page and the datas are corrects
 
     $reservation->append_warning("Veuillez remplir tous les champs correctement.\n");

models.php
@@ -102,7 +102,7 @@ class Person
     function __construct($fullname, $age)
     {
         $this->fullname = htmlspecialchars($fullname);
-        $this->age      = htmlspecialchars($age);
+        $this->age      = intval($age);
     }
 
     /**